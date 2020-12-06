9363288035e9e8795bb937672aea032d382a6f60
Tashila <starbuks1298@gmail.com>
2020-12-06 19:35:04 +0530

    Added service response Generic class

--- dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -13,20 +13,33 @@ namespace dotnet_rpg.Services.CharacterService
             new Character {Id = 1, Name = "Sam"}
         };
 
-        public async Task<List<Character>> GetAllCharacters()
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
         {
-            return characters;
+            var serviceResponse = new ServiceResponse<List<Character>>
+            {
+                Data = characters
+            };
+            return serviceResponse;
         }
 
-        public async Task<Character> GetCharacterById(int id)
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
         {
-            return characters.FirstOrDefault(c => c.Id == id);
+            var serviceResponse = new ServiceResponse<Character>
+            {
+                Data = characters.FirstOrDefault(c => c.Id == id)
+            };
+            return serviceResponse;
         }
 
-        public async Task<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
-            return characters;
+            var serviceResponse = new ServiceResponse<List<Character>>
+            {
+                Data = characters
+            };
+
+            return serviceResponse;
         }
     }
 }
