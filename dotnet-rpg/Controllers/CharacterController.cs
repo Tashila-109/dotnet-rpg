b95d5efd1e212a7cfdea00c11281ed2e1e42526b
Tashila <starbuks1298@gmail.com>
2020-12-06 22:07:01 +0530

    Added Delete Endpoints

--- dotnet-rpg/Controllers/CharacterController.cs
+++ dotnet-rpg/Controllers/CharacterController.cs
@@ -44,5 +44,16 @@ namespace dotnet_rpg.Controllers
             }
             return Ok(response);
         }
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCharacter(int id)
+        {
+            var response = await _characterService.DeleteCharacter(id);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response); 
+        }
     }
 }
