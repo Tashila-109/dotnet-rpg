bb0be560670005fcd11966f74fb587994c13bcdc
Tashila <starbuks1298@gmail.com>
2020-12-09 21:57:00 +0530

    Added sqlite database, and updated api routes

--- dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using dotnet_rpg.Data;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_rpg.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
         private readonly IMapper _mapper;
+        private readonly DataContext _context;
 
         private static List<Character> characters = new List<Character>
         {
@@ -18,37 +21,43 @@ namespace dotnet_rpg.Services.CharacterService
             new Character {Id = 1, Name = "Sam"}
         };
 
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
+            _context = context;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
+            var dbCharacters = await _context.Characters.ToListAsync();
+
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>
             {
-                Data = (characters.Select(c => _mapper.Map<GetCharacterDto>(c))).ToList()
+                Data = (dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c))).ToList()
             };
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+
             var serviceResponse = new ServiceResponse<GetCharacterDto>
             {
-                Data = _mapper.Map<GetCharacterDto>(characters.FirstOrDefault(c => c.Id == id))
+                Data = _mapper.Map<GetCharacterDto>(dbCharacter)
             };
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
-            var character = (_mapper.Map<Character>(newCharacter));
-            character.Id = characters.Max(c => c.Id) + 1;
-            characters.Add(character);
+            var character = _mapper.Map<Character>(newCharacter);
+
+            await _context.Characters.AddAsync(character);
+            await _context.SaveChangesAsync();
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>
             {
-                Data = (characters.Select(c => _mapper.Map<GetCharacterDto>(c))).ToList()
+                Data = _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList()
             };
 
             return serviceResponse;
@@ -60,10 +69,10 @@ namespace dotnet_rpg.Services.CharacterService
 
             try
             {
-                var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+                var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
                 if (character == null)
                 {
-                    throw new Exception("No such character.");
+                    throw new Exception("No such character in the database.");
                 }
 
                 character.Name = updatedCharacter.Name;
@@ -72,6 +81,9 @@ namespace dotnet_rpg.Services.CharacterService
                 character.HitPoints = updatedCharacter.HitPoints;
                 character.Intelligence = updatedCharacter.Intelligence;
                 character.Strength = updatedCharacter.Strength;
+
+                _context.Characters.Update(character);
+                await _context.SaveChangesAsync();
 
                 serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
             }
--- dotnet-rpg/Startup.cs
+++ dotnet-rpg/Startup.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
+using dotnet_rpg.Data;
 using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,7 @@ namespace dotnet_rpg
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("SqliteConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
